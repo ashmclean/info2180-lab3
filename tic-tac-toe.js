7a7089fb414582a22798a0d93c466956c1b94baf
Ashleigh McLean <ashmclean55@gmail.com>
2023-10-22 08:58:54 -0500

    Added an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
   const board = document.getElementById('board');
   const squares = board.querySelectorAll('div');
-  
-  squares.forEach(function(square) {
+  let currentPlayer = 'X';
+  var gameState = ['', '', '', '', '', '', '', '', ''];
+
+  squares.forEach(function(square, index) {
     square.classList.add('square');
+
+    square.addEventListener('click', function() {
+      if (!square.classList.contains('X') && !square.classList.contains('O')) {
+        square.classList.add(currentPlayer);
+        square.textContent = currentPlayer;
+        gameState[index] = currentPlayer;
+
+        if (isGameOver(gameState)) {
+          console.log('Game Over');
+        }
+
+        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+      }
+    });
   });
 });
+
+function isGameOver(gameState) {
+  // Check for a win
+  const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] // Diagonals
+  ];
+
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (gameState[a] && gameState[a] === gameState[b] && gameState[b] === gameState[c]) {
+      return true; // A player has won
+    }
+  }
+
+  // Check for a tie
+  if (gameState.every(square => square === 'X' || square === 'O')) {
+    return true; // It's a tie
+  }
+
+  return false; // Game is not over
+}
